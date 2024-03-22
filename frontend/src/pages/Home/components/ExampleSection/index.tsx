6da06740717d4cb99e9d09f3aabcaaa275e22966
oliveiralecca <oliveiraslc@yahoo.com.br>
2024-03-22 01:58:47 -0300

    feat(home-sections): creates another home section to example

---
+++ frontend/src/pages/Home/components/ExampleSection/index.tsx
@@ -0,0 +1,9 @@
+import { ExampleContainer } from "./styles";
+
+export function ExampleSection() {
+  return (
+    <ExampleContainer>
+      <p>Outra seção de exemplo</p>
+    </ExampleContainer>
+  );
+}
---
+++ frontend/src/pages/Home/components/ExampleSection/styles.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const ExampleContainer = styled.div`
+  background-color: pink;
+`;
