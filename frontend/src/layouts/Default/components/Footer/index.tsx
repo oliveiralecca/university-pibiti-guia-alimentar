9a3a35e156eb9a03462b37f111d1cb9175b36f0f
oliveiralecca <oliveiraslc@yahoo.com.br>
2024-07-04 21:46:17 -0400

    feat(frontend): makes the footer responsive

--- frontend/src/layouts/Default/components/Footer/index.tsx
+++ frontend/src/layouts/Default/components/Footer/index.tsx
@@ -1,8 +1,6 @@
 import { Link } from "react-router-dom";
 
 import emailIcon from "@/assets/email-icon.svg";
-import facebookLogo from "@/assets/facebook-logo.svg";
-import instagramLogo from "@/assets/instagram-logo.svg";
 import logoGuia from "@/assets/logo.png";
 
 import { Img } from "../Header/styles";
@@ -30,25 +28,11 @@ export function Footer() {
       </FooterMenu>
       <Divider />
       <SocialMedia>
-        <MediaButton to="https://www.google.com" target="_blank">
-          <img
-            src={facebookLogo}
-            alt="Logo azul com o F do Facebook"
-            height={40}
-          />
-        </MediaButton>
-        <MediaButton to="https://www.google.com" target="_blank">
-          <img
-            src={instagramLogo}
-            alt="Logo azul simulando uma câmera"
-            height={35}
-          />
-        </MediaButton>
-        <MediaButton to="mailto:teste@email.com" target="_blank">
+        <MediaButton to="mailto:lean@academico.ufs.br" target="_blank">
           <img src={emailIcon} alt="Ícone de carta fechada azul" height={22} />
         </MediaButton>
       </SocialMedia>
-      <small>© 2024 Projeto PIBITI - DNUT e DCOMP</small>
+      <small>© 2024 Projeto PIBITI - DCOMP e DNUT / UFS</small>
     </FooterContainer>
   );
 }
--- frontend/src/layouts/Default/components/Footer/styles.ts
+++ frontend/src/layouts/Default/components/Footer/styles.ts
@@ -1,22 +1,22 @@
 import { NavLink } from "react-router-dom";
-
 import styled from "styled-components";
 
 export const FooterContainer = styled.div`
   background-color: ${({ theme }) => theme.colors.primary.turquoise};
-
   padding: 50px 130px 20px;
-
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  @media (max-width: 768px) {
+    padding: 40px 20px 20px;
+  }
 `;
 
 export const FooterMenu = styled.nav`
   display: flex;
   align-items: center;
   gap: 100px;
-
   margin-top: 50px;
 
   a {
@@ -28,38 +28,56 @@ export const FooterMenu = styled.nav`
   a:hover {
     color: ${({ theme }) => theme.colors.secondary["light-blue"]};
   }
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    gap: 20px;
+    margin-top: 30px;
+  }
 `;
 
 export const Divider = styled.div`
   width: 100%;
   height: 4px;
-
   margin: 40px 0;
-
   background: radial-gradient(
     50% 50% at 50% 50%,
     #91e3ef 0%,
     rgba(50, 121, 79, 0) 100%
   );
+
+  @media (max-width: 768px) {
+    margin: 30px 0;
+  }
 `;
 
 export const SocialMedia = styled.div`
   display: flex;
   gap: 40px;
-
   margin-bottom: 50px;
+
+  @media (max-width: 768px) {
+    gap: 20px;
+    margin-bottom: 30px;
+  }
 `;
 
 export const MediaButton = styled(NavLink)`
   background-color: ${({ theme }) => theme.colors.secondary["light-blue"]};
-
   width: 50px;
   height: 50px;
-
   display: flex;
   align-items: center;
   justify-content: center;
-
   border-radius: 50%;
   box-shadow: 0px 3.238095283508301px 3.238095283508301px 0px #00000040;
+`;
+
+export const Img = styled.img`
+  box-shadow: 0px 3px 4px 0px ${({ theme }) => theme.colors.black[800]}5f;
+  border-radius: 6px;
+
+  @media (max-width: 768px) {
+    height: 30px;
+  }
 `;
