28822a7b879ffb99d08c9c3c05138d92b2fe0351
oliveiralecca <oliveiraslc@yahoo.com.br>
2024-07-04 20:05:15 -0400

    feat(frontend): makes the guide section responsive

--- frontend/src/pages/Home/components/GuiaSection/styles.ts
+++ frontend/src/pages/Home/components/GuiaSection/styles.ts
@@ -2,16 +2,18 @@ import styled from "styled-components";
 
 export const GuiaSectionContainer = styled.div`
   padding: 65px 95px 100px 160px;
+
+  @media (max-width: 768px) {
+    padding: 40px 30px;
+  }
 `;
 
 export const Title = styled.div`
-  width: 50%;
+  width: 465px;
   height: 201px;
-
   margin-left: -160px;
   padding: 30px 0 30px 55px;
   border-radius: 0 20px 20px 0;
-
   background: ${({ theme }) => theme.colors.primary.turquoise};
 
   div {
@@ -19,7 +21,6 @@ export const Title = styled.div`
     flex-direction: column;
     justify-content: center;
     gap: 60px;
-
     width: max-content;
     height: 100%;
 
@@ -39,12 +40,30 @@ export const Title = styled.div`
     font-weight: ${({ theme }) => theme.fontWeight.bold};
     color: ${({ theme }) => theme.colors.black.white};
   }
+
+  @media (max-width: 768px) {
+    height: 100.5px;
+    width: 232.5px;
+    margin-left: -30px;
+    padding: 20px;
+
+    div {
+      gap: 15px;
+    }
+
+    p:nth-child(1) {
+      font-size: ${({ theme }) => theme.fontSize.paragraph1};
+    }
+
+    p:nth-child(2) {
+      font-size: 36.2px;
+    }
+  }
 `;
 
 export const MainContent = styled.div`
   display: flex;
   gap: 90px;
-
   margin: 50px 0;
   align-items: center;
 
@@ -54,11 +73,13 @@ export const MainContent = styled.div`
 
   div:nth-child(2) {
     text-align: center;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
 
     img {
       border-radius: 20px;
       box-shadow: 0px 4px 4px 0px ${({ theme }) => theme.colors.black.primary}40;
-      margin-bottom: 20px;
     }
 
     small {
@@ -66,13 +87,17 @@ export const MainContent = styled.div`
       color: ${({ theme }) => theme.colors.black[600]};
     }
   }
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    gap: 30px;
+  }
 `;
 
 export const CallToAction = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-
   gap: 70px;
 
   p {
@@ -91,5 +116,22 @@ export const CallToAction = styled.div`
     width: fit-content;
     font-size: ${({ theme }) => theme.fontSize.paragraph2};
     font-weight: ${({ theme }) => theme.fontWeight.bold};
+  }
+
+  @media (max-width: 768px) {
+    gap: 40px;
+    p {
+      text-align: center;
+      font-size: ${({ theme }) => theme.fontSize.heading2};
+
+      span {
+        font-size: ${({ theme }) => theme.fontSize.heading1};
+        line-height: 45px;
+      }
+    }
+
+    button {
+      font-size: ${({ theme }) => theme.fontSize.paragraph3};
+    }
   }
 `;
